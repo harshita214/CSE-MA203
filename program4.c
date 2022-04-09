cf095e82f4e82248b899c2cb3da62b26fafd8831
Harshita sharma <79797000+harshita214@users.noreply.github.com>
2022-04-09 20:48:36 +0530

    Create program4.c

---
+++ program4.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<math.h>
+float f(float x)
+{
+    return 3*x + sin(x) - exp(x);
+}
+float df (float x)
+{
+    return 3 + cos(x) - exp(x);
+}
+void main()
+{
+    int itr, maxmitr;
+    float h, x0, x1, allerr;
+    printf("\nEnter x0, allowed error and maximum iterations\n");
+    scanf("%f %f %d", &x0, &allerr, &maxmitr);
+    for (itr=1; itr<=maxmitr; itr++)
+    {
+        h=f(x0)/df(x0);
+        x1=x0-h;
+        printf(" At Iteration no. %3d, x = %9.6f\n", itr, x1);
+        if (fabs(h) < allerr)
+        {
+            printf("After %3d iterations, root = %8.6f\n", itr, x1);
+            return 0;
+        }
+        x0=x1;
+    }
+    printf(" The required solution does not converge or iterations are insufficient\n");
+    return 1;
+}
