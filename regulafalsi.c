8b4b6242a367557f2544a456c4d57118f532618c
Harshita sharma <79797000+harshita214@users.noreply.github.com>
2022-04-18 20:20:50 +0530

    Create regulafalsi.c

---
+++ regulafalsi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#define e 0.001
+#define f(x) x*x*x - 4*x + 1 
+void main(){
+    int i=0;
+    float x0,x1,x2,f0,f1,f2;
+    printf("Enter the values of x0 and x1 : ");
+    scanf("%f %f", &x0, &x1);
+    
+    do{
+        f0 = f(x0);
+        f1 = f(x1);
+        x2 = ((x0*f1)-(x1*f1))/(f1-f0);
+        
+        f2 = f(x2);
+        if(f0*f2 <0){
+            x1 = x2;
+            f1 = f2;
+        }
+        else{
+            x0 = x2;
+            f0 = f2;
+        }
+        i++;
+        printf("No of iterations %d \t", i);
+        printf("Roots %f \t", x2);
+        printf("Value of function %f ", f2);
+        
+    }while(fabs(f2)>0);
+    getch();
+}
